2b0e507e6bf82cd424f90a968ad66f1bd49d1ce1
phucmanh1310 <128921724+phucmanh1310@users.noreply.github.com>
2025-05-21 14:58:10 +0700

    .

--- WebBanHangOnline/Hub/OrderHub.cs
+++ WebBanHangOnline/Hub/OrderHub.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
+using WebBanHangOnline.Models.ViewModels;
 
 namespace WebBanHangOnline.Hubs
 {
     public class OrderHub : Hub
     {
-        // Gửi đi khi có đơn mới
+        // Phát sự kiện khi có đơn hàng mới
         public static void BroadcastNewOrder(string orderCode)
         {
             var context = GlobalHost.ConnectionManager.GetHubContext<OrderHub>();
             context.Clients.All.newOrder(orderCode);
         }
 
-        // Gửi đi khi trạng thái đơn thay đổi
+        // Phát sự kiện khi trạng thái đơn thay đổi
         public static void BroadcastStatusChange(string orderCode, string newStatus)
         {
             var context = GlobalHost.ConnectionManager.GetHubContext<OrderHub>();
             context.Clients.All.updateOrderStatus(orderCode, newStatus);
+        }
+
+        // Phát sự kiện cập nhật toàn bộ số liệu thống kê realtime
+        public static void BroadcastOrderStats(OrderStatisticsViewModel stats)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<OrderHub>();
+            context.Clients.All.updateOrderStats(stats);
         }
     }
 }
